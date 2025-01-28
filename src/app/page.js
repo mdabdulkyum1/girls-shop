ec7241dad8287e5e143725f7576129c1b0d4497a
mdabdulkyum1 <kyummdabdul@gmail.com>
2025-01-28 11:44:20 +0600

    created home page

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@
 export default function Home() {
   return (
       <div>
+        
            <h1 className="text-green-600 text-7xl">Home Page</h1>
            <button class="bg-primary text-white rounded-lg p-2">
   Shop Now
